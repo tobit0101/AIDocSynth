4a28701cd13fb17c470879b05a88e5ec0f7afd9a
Tobias Müller <tobidex@outlook.com>
2025-06-10 19:48:56 +0200

    Add resource import and fix indentation

--- aidocsynth/app.py
+++ aidocsynth/app.py
@@ -1,9 +1,7 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QSystemTrayIcon, QMenu
 from PySide6.QtGui import QIcon
 import sys
-
-# Wichtig: Importiert die kompilierten Ressourcen (Icons, etc.)
-from aidocsynth.ui import qrc_resources
+from .ui import qrc_resources
 
 def main():
     app = QApplication(sys.argv)
@@ -12,10 +10,12 @@ def main():
     win.resize(600, 400)
     win.show()
 
-            icon = QIcon(":/app.png")            # Ressource ab Schritt 7
+    icon = QIcon(":/app.png")
     tray = QSystemTrayIcon(icon, app)
-    menu = QMenu(); menu.addAction("Beenden", app.quit)
-    tray.setContextMenu(menu); tray.show()
+    menu = QMenu()
+    menu.addAction("Beenden", app.quit)
+    tray.setContextMenu(menu)
+    tray.show()
 
     sys.exit(app.exec())
 
