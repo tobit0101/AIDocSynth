83ff8f535c182bed8ab93b5b69794e181a119ac5
Tobias Müller <tobidex@outlook.com>
2025-06-11 00:14:16 +0200

    Fix: Enable async task execution in Worker

--- aidocsynth/utils/worker.py
+++ aidocsynth/utils/worker.py
@@ -1,20 +1,29 @@
-from PySide6.QtCore import QRunnable, QObject, Signal
-import asyncio, traceback
+from PySide6.QtCore import QObject, QRunnable, Signal, Slot
+import asyncio
+import inspect
+import traceback
 
 class WorkerSignals(QObject):
     finished = Signal(object)
     error    = Signal(str)
+    result   = Signal(object)
 
 class Worker(QRunnable):
-    def __init__(self, coro, *args):
+    def __init__(self, fn, *args, **kwargs):
         super().__init__()
-        self.coro, self.args = coro, args
+        self.fn, self.args, self.kwargs = fn, args, kwargs
         self.sig = WorkerSignals()
 
+    @Slot()
     def run(self):
         try:
-            loop = asyncio.new_event_loop(); asyncio.set_event_loop(loop)
-            res  = loop.run_until_complete(self.coro(*self.args))
-            self.sig.finished.emit(res)
-        except Exception:
-            self.sig.error.emit(traceback.format_exc())
+            if inspect.iscoroutinefunction(self.fn):
+                loop = asyncio.new_event_loop(); asyncio.set_event_loop(loop)
+                result = loop.run_until_complete(self.fn(*self.args, **self.kwargs))
+            else:
+                result = self.fn(*self.args, **self.kwargs)
+            self.sig.result.emit(result)
+        except Exception as e:
+            self.sig.error.emit(str(e))
+        finally:
+            self.sig.finished.emit(None)
